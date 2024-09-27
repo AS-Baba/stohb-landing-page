5002508f7e413fac77307e4bcbdb707528c6d08b
Abubakar Salihu <salihuabubakar557@gmail.com>
2024-09-27 23:37:54 +0100

    Changed an icon in Products section

--- src/app/_components/products.tsx
+++ src/app/_components/products.tsx
@@ -24,9 +24,9 @@ export const Products = () => {
                 </div>
                 <div className={"flex  gap-2"}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                         stroke="green" className="size-6">
+                         stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M21 12a2.25 2.25 0 0 0-2.25-2.25H15a3 3 0 1 1-6 0H5.25A2.25 2.25 0 0 0 3 12m18 0v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 9m18 0V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v3"/>
+                              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                     </svg>
 
                     <div>
