1a0412cfb9d84ed206dff93f44fd726a33586ba2
Abubakar Salihu <salihuabubakar557@gmail.com>
2024-10-03 09:17:26 +0100

    Hero section refactored

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -1,21 +1,28 @@
+import Image from "next/image";
+
 export const HeroSection = () =>{
     return (
         <>
             <div
-                className="hero-section text-left flex justify-items-center items-center h-[400px] md:min-h-[500px]  font-[family-name:var(--font-geist-sans)]  px-[56px]  ">
+                className="flex justify-between items-center max-sm:bg-[#ffffff] hero-section text-left  h-[200px] md:min-h-[500px]  font-[family-name:var(--font-geist-sans)]  px-[56px]  ">
                 <div className={"flex flex-col justify-start items-start gap-4 "}>
-                    <div className={"flex flex-col gap-4 md:w-[600px] "}>
-                        <h1 className={"text-[#ffffff] text-xl md:text-5xl font-[500] capitalize antialiased"}>Manage <span className={"text-emerald-400 "}> payment
+                    <div className={"flex flex-col gap-4 w-[150px] md:w-[600px] "}>
+                        <h1 className={"text-[#111] text-[12px] md:text-5xl font-[500] capitalize antialiased"}>Manage <span className={"text-emerald-900 "}> payment
                             & <br/>
                             expenses </span> on the fly like never before</h1>
-                        <p className={"md:text-[26px] text-[16px] text-[#DADADAFF]  antialiased"}>Stohb helps businesses manage and audit financial transactions, manage customers and in a very to adopt manner.</p>
+                        <p className={"md:text-[26px] text-[10px] text-[#111]  antialiased"}>Stohb helps businesses manage and audit financial transactions, manage customers and in a very to adopt manner.</p>
                     </div>
                     <button type={"button"}
-                            className={"bg-emerald-700 text-white text-sm tracking-wider px-[16px] py-[8px]  rounded-full"}>Get
-                        Started
+                            className={"bg-emerald-700 text-white text-[10px] md:text-sm tracking-wider px-[8px] py-[4px] md:px-[16px] md:py-[8px]  rounded-full"}>Get
+                        Started Now
                     </button>
                 </div>
-
+                <Image
+                    src={"/assets/Hero-image.png"}
+                    alt={"..."}
+                    width={300}
+                    height={300}
+                    className={"max-sm:w-[100px] max-sm:h-[150px]"} />
             </div>
         </>
     );
--- src/app/_components/ui/header.tsx
+++ src/app/_components/ui/header.tsx
@@ -2,11 +2,11 @@
 export const  Header = () =>{
     return (
         <div className={"flex justify-between items-center header bg- h-[60px] px-[16px] py-[8px] sticky top-0"}>
-            <h1 className={"text-2xl text-emerald-950 antialiased"}>STOHB</h1>
+            <h1 className={"text-[14px] md:text-2xl text-emerald-950 antialiased"}>STOHB</h1>
             <div className={"flex justify-center items-center gap-4"} >
                 <p className={"max-sm:hidden text-sm text-gray-500 antialiased"}>Terms</p>
                 <p className={"max-sm:hidden text-sm text-gray-500  antialiased"}>Privacy Policy</p>
-                <button type={"button"} className={"bg-emerald-700 text-white text-sm tracking-wider px-[16px] py-[8px]  rounded-full"}>Get Started
+                <button type={"button"} className={"bg-emerald-700 text-white text-[10px] md:text-sm tracking-wider  px-[8px] py-[4px]  md:px-[16px] md:py-[8px]  rounded-full"}>Get Started
                 </button>
             </div>
 
