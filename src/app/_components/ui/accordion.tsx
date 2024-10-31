73a6c1f10f9c462e99c56760c121f271076575bf
Abubakar Salihu <salihuabubakar557@gmail.com>
2024-10-31 11:36:22 +0100

    linted

--- src/app/_components/ui/accordion.tsx
+++ src/app/_components/ui/accordion.tsx
@@ -1,13 +1,18 @@
 // components/Accordion.js
 import { useState } from "react";
 
-function AccordionItem({ title, content }: any) {
+interface accordionInterface {
+    title: string,
+    content: string
+}
+
+function AccordionItem({ title, content }:accordionInterface) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="w-full ">
       <button
-        className="w-full text-left py-4 px-4 ma:px-6 text-[14px] md:text-lg font-[400] md:font-[600] flex justify-between items-center gap-2  text-[#002c15ff] "
+        className="w-full text-left py-4 px-4 ma:px-6 text-[1px] md:text-lg font-[400] md:font-[600] flex justify-between items-center gap-2  text-[#002c15ff] "
         onClick={() => setIsOpen(!isOpen)}
       >
         {title}
@@ -56,7 +61,7 @@ export default function Accordion() {
   return (
     <div className="w-full mx-auto rounded-lg overflow-hidden">
       {data.map((item, index) => (
-        <div className="m-3 border rounded-lg">
+        <div key={index} className="m-3 border rounded-lg">
           <AccordionItem
             key={index}
             title={item.title}
